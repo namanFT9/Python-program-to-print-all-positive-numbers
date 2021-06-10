88efe0b256c8eec6d3991b0bdce9478399cea24c
namanFT9 <85551435+namanFT9@users.noreply.github.com>
2021-06-10 22:46:34 +0530

    Add files via upload

---
+++ print postive numbers from the list.py
@@ -0,0 +1,25 @@
+# list 1
+list1 = (12, -7, 5, 64, -14)
+print ("the output is :")
+
+for number in list1:
+    if number >= 0:#check if number is greater than 0 i.e positive 
+     print (number, end= " ")
+
+
+print(" ")
+print(" ")
+"""
+To give the spacing between
+list1 and list 2 we've
+printed the blank space twice
+"""
+
+
+# list 2
+list2 = [12, 14, -95, 3]
+print ("the output is :")
+
+for num in list2:
+    if num >= 0:
+     print ( num, end= " ")
